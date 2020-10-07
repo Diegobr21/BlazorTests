8bef62baa5e62de72308362869a8c6cc5bed7d83
Diegobr21 <jdiegobrana@gmail.com>
2020-10-07 13:40:27 -0500

    Add project files.

---
+++ Elecritic/CategoriasLogic.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public class CategoriasLogic
+{
+	public string Title { get; set; }
+}
---
+++ Elecritic/Product.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Product
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public float Rating { get; set; }
+}
