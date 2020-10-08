10e983a897a0f974a430cafe14c9a4c24ebcca60
Diegobr21 <jdiegobrana@gmail.com>
2020-10-08 16:24:17 -0500

    Review C# class created

---
+++ Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Review
+{
+	public string AuthorId { get; set; }
+
+	public string ReviewText {get; set;}
+
+	public float RatingProduct { get; set; }
+
+	public string ProductId { get; set; }
+	
+}
