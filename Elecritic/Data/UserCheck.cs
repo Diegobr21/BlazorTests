e886b94ed72ff9fc4ce3642004ae0462ccc0def5
Diegobr21 <jdiegobrana@gmail.com>
2020-10-28 15:52:04 -0600

    Login Page created + login button on navbar

---
+++ Elecritic/Data/UserCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elecritic.Data
+{
+    public class UserCheck
+    {
+        [Required(ErrorMessage = "Este campo no puede estar vacío")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede estar vacío")]
+        public string Password { get; set; }
+
+
+        public UserCheck()
+        {
+        }
+        public UserCheck(string email, string password)
+        {
+            Email = email;
+            Password = password;
+
+        }
+    }
+}
